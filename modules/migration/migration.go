7e023297e489de68a11f957b9b6b36bc481416fe
Widya Ade Bagus <dewidyabagus@gmail.com>
2021-11-13 22:29:13 +0700

    add admin table migration

--- modules/migration/migration.go
+++ modules/migration/migration.go
@@ -3,9 +3,10 @@ package migration
 import (
 	"gorm.io/gorm"
 
+	"RESTful/modules/admin"
 	"RESTful/modules/user"
 )
 
 func TableMigration(db *gorm.DB) {
-	db.AutoMigrate(&user.User{})
+	db.AutoMigrate(&user.User{}, &admin.Admin{})
 }
