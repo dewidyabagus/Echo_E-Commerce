cb02125344af7379d34793bf1d23d10a0896d3df
Widya Ade Bagus <dewidyabagus@gmail.com>
2021-11-14 13:48:02 +0700

    add dummy data for merchant and outlet

---
+++ modules/merchant/dummy.sql
@@ -0,0 +1,3 @@
+INSERT INTO "merchants" ("id", "name", "created_at", "updated_at") 
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'CV MAJU JAYA', '2021-11-14', '2021-11-14'),
+    ('6ecd8c99-4036-403d-bf84-cf8400f67836', 'PT AMARAH JAYA', '2021-11-14', '2021-11-14');
---
+++ modules/outlet/dummy.sql
@@ -0,0 +1,3 @@
+INSERT INTO "outlets" ("id", "merchant_id", "name", "created_at", "updated_at")
+    VALUES('a81bc81b-dead-4e5d-abff-90865d1e13b1', '40e6215d-b5c6-4896-987c-f30f3678f608', 'TOKO BAJU SERGA 5000', '2021-11-14', '2021-11-14'),
+    ('a81bc81b-dead-4e5d-abff-90865d1e13b2', '6ecd8c99-4036-403d-bf84-cf8400f67836', 'TOKO ELEKTRONIK JOKA', '2021-11-14', '2021-11-14');
