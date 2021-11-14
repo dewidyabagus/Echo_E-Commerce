ac1d217c2e0b2b8e012ab3843b761ec4883894cd
Widya Ade Bagus <dewidyabagus@gmail.com>
2021-11-14 09:05:30 +0700

    add migration table dummy merchat, dummy outlet and product

--- modules/migration/migration.go
+++ modules/migration/migration.go
@@ -4,9 +4,12 @@ import (
 	"gorm.io/gorm"
 
 	"RESTful/modules/admin"
+	"RESTful/modules/merchant"
+	"RESTful/modules/outlet"
+	"RESTful/modules/product"
 	"RESTful/modules/user"
 )
 
 func TableMigration(db *gorm.DB) {
-	db.AutoMigrate(&user.User{}, &admin.Admin{})
+	db.AutoMigrate(&admin.Admin{}, &merchant.Merchant{}, &outlet.Outlet{}, &user.User{}, &product.Product{})
 }
